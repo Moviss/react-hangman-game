41884b761a0cd67875f246b85ecd71e79ff97f22
Marcin Lubowicz <marcin.lubowicz@gmail.com>
2018-12-12 23:16:22 +0100

    Finished visual Hangman Component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.scss';
 
+import Hangman from './components/Hangman';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="App-playground">
-
+            <Hangman />
         </div>
       </div>
     );
